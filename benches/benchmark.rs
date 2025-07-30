f1535beabfe251733b6d96e16fc39d07fbd573c8
fmcgg <github@fmc.gg>
2025-07-30 18:44:29 +0200

    Noiz mirror benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,93 +1,100 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use fmc_noise::Noise;
-//use simdnoise::{avx2, sse2};
 
-fn d1(c: &mut Criterion) {
-    let mut group = c.benchmark_group("gradient_1d");
+const SIZE_2D: usize = 1024;
+const SIZE_3D: usize = 101;
+
+fn perlin_2d(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fmc_noise/2d");
+
+    let noise = Noise::perlin(0.01);
+    group.bench_function("perlin", |b| {
+        b.iter(|| noise.generate_2d(0.0, 0.0, SIZE_2D, SIZE_2D))
+    });
+
+    let noise = Noise::perlin(0.01).fbm(2, 0.5, 2.0);
+    group.bench_function("perlin fbm 2 octaves", |b| {
+        b.iter(|| noise.generate_2d(0.0, 0.0, SIZE_2D, SIZE_2D))
+    });
+
+    let noise = Noise::perlin(0.01).fbm(8, 0.5, 2.0);
+    group.bench_function("perlin fbm 8 octaves", |b| {
+        b.iter(|| noise.generate_2d(0.0, 0.0, SIZE_2D, SIZE_2D))
+    });
+
+    group
+        .warm_up_time(std::time::Duration::from_millis(500))
+        .measurement_time(std::time::Duration::from_secs(5));
+}
+
+fn simplex_2d(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fmc_noise/2d");
 
     let noise = Noise::simplex(0.01);
-    group.bench_function("lib", |b| b.iter(|| noise.generate_1d(0.0, 1000000)));
+    group.bench_function("simplex", |b| {
+        b.iter(|| noise.generate_2d(0.0, 0.0, SIZE_2D, SIZE_2D))
+    });
 
-    //let settings = simdnoise::NoiseBuilder::gradient_1d(1000).wrap();
-    //group.bench_function("simdnoise", move |b| {
-    //    b.iter(|| unsafe { avx2::get_1d_noise(&settings) })
-    //});
+    let noise = Noise::simplex(0.01).fbm(2, 0.5, 2.0);
+    group.bench_function("simplex fbm 2 octaves", |b| {
+        b.iter(|| noise.generate_2d(0.0, 0.0, SIZE_2D, SIZE_2D))
+    });
+
+    let noise = Noise::simplex(0.01).fbm(8, 0.5, 2.0);
+    group.bench_function("simplex fbm 8 octaves", |b| {
+        b.iter(|| noise.generate_2d(0.0, 0.0, SIZE_2D, SIZE_2D))
+    });
+
     group
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(1))
+        .warm_up_time(std::time::Duration::from_millis(500))
         .measurement_time(std::time::Duration::from_secs(5));
 }
 
-fn d2(c: &mut Criterion) {
-    let mut group = c.benchmark_group("gradient_2d");
+fn perlin_3d(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fmc_noise/3d");
+
+    let noise = Noise::perlin(0.01);
+    group.bench_function("perlin", |b| {
+        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, SIZE_3D, SIZE_3D, SIZE_3D))
+    });
+
+    let noise = Noise::perlin(0.01).fbm(2, 0.5, 2.0);
+    group.bench_function("perlin fbm 2 octaves", |b| {
+        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, SIZE_3D, SIZE_3D, SIZE_3D))
+    });
+
+    let noise = Noise::perlin(0.01).fbm(8, 0.5, 2.0);
+    group.bench_function("perlin fbm 8 octaves", |b| {
+        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, SIZE_3D, SIZE_3D, SIZE_3D))
+    });
+
+    group
+        .warm_up_time(std::time::Duration::from_millis(500))
+        .measurement_time(std::time::Duration::from_secs(5));
+}
+
+fn simplex_3d(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fmc_noise/3d");
+
     let noise = Noise::simplex(0.01);
-    group.bench_function("lib", move |b| {
-        b.iter(|| noise.generate_2d(0.0, 0.0, 1000, 1000))
+    group.bench_function("simplex", |b| {
+        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, SIZE_3D, SIZE_3D, SIZE_3D))
     });
 
-    //let setting = simdnoise::NoiseBuilder::gradient_2d(3840, 2160).wrap();
-    //group.bench_function("simdnoise", move |b| {
-    //    b.iter(|| unsafe { avx2::get_2d_noise(&setting) })
-    //});
-    group
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(1))
-        .measurement_time(std::time::Duration::from_secs(5));
-}
-
-fn d3(c: &mut Criterion) {
-    let mut group = c.benchmark_group("gradient_3d");
-    let noise = Noise::simplex(0.01);
-    group.bench_function("lib", move |b| {
-        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, 100, 100, 100))
+    let noise = Noise::simplex(0.01).fbm(2, 0.5, 2.0);
+    group.bench_function("simplex fbm 2 octaves", |b| {
+        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, SIZE_3D, SIZE_3D, SIZE_3D))
     });
 
-    //let setting = simdnoise::NoiseBuilder::gradient_3d(16, 16, 16).wrap();
-    //group.bench_function("simdnoise", move |b| {
-    //    b.iter(|| unsafe { avx2::get_3d_noise(&setting) })
-    //});
-    group
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(1))
-        .measurement_time(std::time::Duration::from_secs(5));
-}
-
-fn fbm_3d(c: &mut Criterion) {
-    let mut group = c.benchmark_group("fbm_3d");
-
-    let freq = 1.0 / 2.0f32.powi(8);
-    let high = Noise::perlin(freq).fbm(4, 0.5, 2.0);
-    let low = Noise::perlin(freq).fbm(4, 0.5, 2.0);
-    let noise = Noise::perlin(freq)
-        .fbm(8, 0.5, 2.0)
-        .range(0.1, -0.1, high, low)
-        .mul(Noise::constant(2.0));
-    group.bench_function("lib", move |b| {
-        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, 16, 16, 16))
+    let noise = Noise::simplex(0.01).fbm(8, 0.5, 2.0);
+    group.bench_function("simplex fbm 8 octaves", |b| {
+        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, SIZE_3D, SIZE_3D, SIZE_3D))
     });
 
     group
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(1))
+        .warm_up_time(std::time::Duration::from_millis(500))
         .measurement_time(std::time::Duration::from_secs(5));
 }
 
-fn add_3d(c: &mut Criterion) {
-    let mut group = c.benchmark_group("add_3d");
-
-    let noise = Noise::simplex(0.01).fbm(3, 1.0, 1.0);
-    let noise2 = Noise::simplex(0.01).fbm(3, 1.0, 1.0);
-    let noise = noise.add(noise2);
-    group.bench_function("lib", move |b| {
-        b.iter(|| noise.generate_3d(0.0, 0.0, 0.0, 100, 100, 100))
-    });
-
-    group
-        .sample_size(10)
-        .warm_up_time(std::time::Duration::from_millis(1))
-        .measurement_time(std::time::Duration::from_secs(5));
-}
-
-criterion_group!(benches, d1, d2, d3, fbm_3d, add_3d);
-//criterion_group!(benches, fbm_3d);
+criterion_group!(benches, perlin_2d, simplex_2d, perlin_3d, simplex_3d);
 criterion_main!(benches);
